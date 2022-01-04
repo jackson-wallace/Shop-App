65ca515235cd1d07ba82fe3b104abbf85cbab86d
jackson-wallace <82778887+jackson-wallace@users.noreply.github.com>
2022-01-03 20:53:47 -0500

    added ProductItem styling

---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,27 @@
+import { Platform, Text } from "react-native";
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { createAppContainer } from "react-navigation";
+import { Ionicons } from "@expo/vector-icons";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+
+import ProductOverviewScreen from "../screens/shop/ProductOverviewScreen";
+import Colors from "../constants/Colors";
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductOverviewScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+    },
+  }
+);
+
+export default createAppContainer(ProductsNavigator);
